7e36535a302a926d6bf6b61205154a7e6a655a29
yisaphilip5-tech <yisaphilip5@gmail.com>
2026-09-19 22:52:02 +0100

    Update MainActivity.java

--- app/src/main/java/com/kingdomlife/app/MainActivity.java
+++ app/src/main/java/com/kingdomlife/app/MainActivity.java
@@ -59,6 +59,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+      prefs = getSharedPreferences("KingdomLifePrefs", MODE_PRIVATE);
 
         LinearLayout main = new LinearLayout(this);
         main.setOrientation(LinearLayout.VERTICAL);
