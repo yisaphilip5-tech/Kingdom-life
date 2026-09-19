c4fd6bfa9c174e694aaa3f0eecf19957d768374f
yisaphilip5-tech <yisaphilip5@gmail.com>
2026-09-19 22:40:56 +0100

    Update MainActivity.java

--- app/src/main/java/com/kingdomlife/app/MainActivity.java
+++ app/src/main/java/com/kingdomlife/app/MainActivity.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.content.SharedPreferences;
 
 public class MainActivity extends Activity {
 
